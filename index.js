85f6b530f0c08d4e32301331a65dd4c389488410
eternal <73202350+slow@users.noreply.github.com>
2021-07-08 16:14:52 +0100

    little rewrite to avoid crashing in search results menu

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Plugin } = require('powercord/entities');
 const { getModule, constants, messages } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
 
 const Message = getModule(m => m.default?.displayName == 'Message', false);
 const Permissions = getModule(['getChannelPermissions'], false);
@@ -9,22 +10,26 @@ const { getCurrentUser } = getModule(['getCurrentUser'], false);
 module.exports = class QuickDelete extends Plugin {
    startPlugin() {
       inject('quick-delete', Message, 'default', (args, res) => {
-         if (!args[0]?.childrenAccessories?.props) return res;
-         const message = args[0].childrenAccessories.props.message;
-         const channel = args[0].childrenAccessories.props.channel;
-         const deletePerm = Permissions.can(constants.Permissions.MANAGE_MESSAGES, channel);
+         let instance = findInReactTree(res, r => r?.message && r?.channel);
 
-         if (res?.props) {
-            let old = res.props.children.props.onClick
-            res.props.children.props.onClick = function (e) {
-               old(e)
-               if (e?.ctrlKey && !(deletePerm || getCurrentUser().id === message.author.id)) return;
+         if (instance?.message && instance?.channel) {
+            const { message, channel } = instance;
+            const canDelete = (
+               Permissions.can(constants.Permissions.MANAGE_MESSAGES, channel) ||
+               getCurrentUser().id === message.author.id
+            );
 
-               if (e?.ctrlKey) {
-                  messages.deleteMessage(channel.id, message.id);
-               }
+            let props = res.props.children.props;
+
+            props.oldOnClick = props.onClick;
+            props.onClick = (e) => {
+               if (props.oldOnClick) props.oldOnClick(e);
+               if (!e.ctrlKey || !canDelete) return;
+
+               messages.deleteMessage(channel.id, message.id);
             };
-         }
+         };
+
          return res;
       });
    }
